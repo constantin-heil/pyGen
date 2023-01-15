59e8dcb848a7b32496c0a9d51c13281207b6a14d
Constantin Heil <constantin.heil@gmail.com>
2023-01-15 21:18:18 +0100

    structure

--- pygen/storage_helper.py
+++ pygen/storage_helper.py
@@ -20,15 +20,15 @@ class provisionStorage:
 
         self.storage_client = StorageManagementClient(credential, subscription_id)
 
-        if not storagename:
-            storagename = f"storage_account_{random.randint(0, 10000:04}"
+        if not self.sa_name:
+            self.sa_name = f"storageaccount{random.randint(0, 10000):04}"
 
         availability_res = self.storage_client.storage_accounts.check_name_availability(
-            {"name": storagename}
+            {"name": self.sa_name}
         )
 
         if not availability_res.name_available:
-            print(f"ERROR: Name {storagename} is already taken")
+            print(f"ERROR: Name {self.sa_name} is already taken")
             exit(1)
         
         sa_kwargs = {
@@ -46,7 +46,7 @@ class provisionStorage:
 
         self.status_dic = {
             "SA_name": sa_result.name,
-            "SA_region": sa_result.location
+            "SA_region": sa_result.location,
             "RG": self.rg_name
         }
 
@@ -57,16 +57,21 @@ class provisionStorage:
             "secondary": access_keys.keys[1].value
         }
 
-    def provision_blob_container(self, cont_name):
+    def provision_blob_container(self, cont_name = None):
         """
         Provision blob container
         """
+        if not cont_name:
+            cont_name = f"container{random.randint(0, 10000):04}"
+
         cont_result = self.storage_client.blob_containers.create(
             self.rg_name,
             self.sa_name,
             cont_name,
             {}
         )
+
+        return cont_name
 
     
         
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'pygen',
+      version = '0.1',
+      description = "Tools for submitting jobs to Microsoft Genomics service",
+      url = "https://github.com/constantin-heil/pyGen",
+      author = "Constantin Heil",
+      author_email = "constantin.heil@gmail.com",
+      packages = ['pygen'])
